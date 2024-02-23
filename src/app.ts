0d4f617449c52067aa6fb2c32ab1b48b5e6ea435
mdArifulIslam12 <mdarif767778@gmail.com>
2024-02-23 18:11:54 +0600

    husky lini-stage eslint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-import cors from "cors";
-import httpStatus from "http-status";
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import httpStatus from 'http-status';
 const app: Application = express();
 
 app.use(cors());
@@ -10,28 +10,16 @@ app.use(express.json());
 // app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("This is our tour backEnd");
-});
-app.get("/tour/v1", (req, res) => {
-  res.send("This is our tour");
-});
-app.get("/api/v1", (req, res) => {
-  res.send({
-    name: "backend",
-  });
-});
-
 // handle not route
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: "Not Found",
+    message: 'Not Found',
     errorMessages: [
       {
         path: req.originalUrl,
-        message: "API Not Found",
+        message: 'API Not Found',
       },
     ],
   });
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
 const port = 3000;
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log("Database is connected successfully");
+    console.log('Database is connected successfully');
     app.listen(port, () => {
       console.log(`Application listening on port ${port}`);
     });
-  } catch (error) {}
+  } catch (err) {
+    console.log('Failed to connect database', err);
+  }
 }
 
 bootstrap();

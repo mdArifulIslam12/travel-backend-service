3cb22665e05e0c8a4922cfa4d5266984ff27a518
mdArifulIslam12 <mdarif767778@gmail.com>
2024-02-23 21:24:32 +0600

    development

--- dist/server.js
+++ dist/server.js
@@ -1,31 +1,56 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const app_1 = __importDefault(require("./app"));
-const index_1 = __importDefault(require("./config/index"));
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const mongoose_1 = __importDefault(require('mongoose'));
+const app_1 = __importDefault(require('./app'));
+const index_1 = __importDefault(require('./config/index'));
 const port = 3000;
 function bootstrap() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield mongoose_1.default.connect(index_1.default.database_url);
-            console.log("Database is connected successfully");
-            app_1.default.listen(port, () => {
-                console.log(`Application listening on port ${port}`);
-            });
-        }
-        catch (error) { }
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    try {
+      yield mongoose_1.default.connect(index_1.default.database_url);
+      console.log('Database is connected successfully');
+      app_1.default.listen(port, () => {
+        console.log(`Application listening on port ${port}`);
+      });
+    } catch (error) {}
+  });
 }
 bootstrap();

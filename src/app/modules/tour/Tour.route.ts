ef5fa3a0320b2444e4b76f44f28eefe6642f0bae
mdArifulIslam12 <mdarif767778@gmail.com>
2024-03-11 15:43:38 +0600

    push

--- src/app/modules/tour/Tour.route.ts
+++ src/app/modules/tour/Tour.route.ts
@@ -3,9 +3,9 @@ import { TourController } from './Tour.Controller';
 
 const tourRouter = Router();
 
+tourRouter.get('/:id', TourController.findSingleDataFromDb);
 tourRouter.post('/create-tour', TourController.createTour);
 tourRouter.get('/', TourController.findDataFromDb);
-tourRouter.get('/:id', TourController.findSingleDataFromDb);
 tourRouter.patch('/:id', TourController.findSingleDataFromDb);
 tourRouter.delete('/:id', TourController.findSingleDataFromDb);
 
